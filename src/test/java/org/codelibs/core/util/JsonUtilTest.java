164b3c290e88113958379691a9fd45df7e46c6f8
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-07-31 09:56:16 +0900

    add JsonUtil.

---
+++ src/test/java/org/codelibs/core/util/JsonUtilTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2004-2010 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.core.util;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author shinsuke
+ *
+ */
+public class JsonUtilTest {
+    @Test
+    public void escape() {
+        assertThat(JsonUtil.escape("abc123あア亜"), is("abc123あア亜"));
+        assertThat(JsonUtil.escape("\\\"/\b\t\n\f\r\0"),
+                is("\\\\\\\"\\/\\b\\t\\n\\f\\r\\u0000"));
+    }
+}
