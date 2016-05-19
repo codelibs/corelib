443df157f433c5e965b69c55a1e7b2d7b0c6d400
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-05-19 13:26:40 +0900

    add StreamUtil

---
+++ src/main/java/org/codelibs/core/stream/StreamUtil.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2012-2016 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.core.stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamUtil {
+    private StreamUtil() {
+        // nothing
+    }
+
+    @SafeVarargs
+    public static <T> StreamOf<T> stream(final T... values) {
+        return new StreamOf<>(() -> values != null ? Arrays.stream(values)
+                : Collections.<T> emptyList().stream());
+    }
+
+    public static StreamOf<String> split(final String value, final String regex) {
+        return stream(value == null ? null : value.split(regex));
+    }
+
+    public static <K, V> StreamOf<Map.Entry<K, V>> stream(final Map<K, V> map) {
+        return new StreamOf<>(() -> map != null ? map.entrySet().stream()
+                : Collections.<K, V> emptyMap().entrySet().stream());
+    }
+
+    public static class StreamOf<T> {
+
+        private final Supplier<Stream<T>> supplier;
+
+        public StreamOf(final Supplier<Stream<T>> supplier) {
+            this.supplier = supplier;
+        }
+
+        public void of(final Consumer<Stream<T>> consumer) {
+            try (Stream<T> stream = supplier.get()) {
+                consumer.accept(stream);
+            }
+        }
+
+        public <R> R get(final Function<Stream<T>, R> consumer) {
+            try (Stream<T> stream = supplier.get()) {
+                return consumer.apply(stream);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/codelibs/core/stream/StreamUtilTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012-2016 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.core.stream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class StreamUtilTest extends TestCase {
+
+    public void test_ofValues() {
+        String[] values = { "value1", "value2" };
+        StreamUtil.stream(values[0], values[1]).of(s -> {
+            Object[] array = s.toArray();
+            for (int i = 0; i < 2; i++) {
+                assertEquals(values[i], array[i]);
+            }
+        });
+    }
+
+    public void test_ofNull() {
+        assertEquals(0, (int) StreamUtil.stream().get(s -> s.toArray().length));
+        Object[] o = {};
+        assertEquals(0, (int) StreamUtil.stream(o).get(s -> s.toArray().length));
+        Map<Object, Object> map = new HashMap<Object, Object>();
+        assertEquals(0,
+                (int) StreamUtil.stream(map).get(s -> s.toArray().length));
+    }
+
+    public void test_ofMap() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        StreamUtil.stream(map).of(
+                s -> s.forEach(m -> assertEquals(map.get(m.getKey()),
+                        m.getValue())));
+    }
+
+}
